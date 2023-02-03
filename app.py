b9bf6817453bc5d90a11da3d893df5127e1954a9
Oliver Cutting <oliver.cutting@gmail.com>
2023-02-03 15:20:23 +0000

    create habit database model

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 import config
 from flask import Flask, flash, redirect, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
 from forms import HabitInputForm
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = config.SECRET_KEY
+app.config["SQLALCHEMY_DATABASE_URI"] = config.SQLALCHEMY_DATABASE_URI
+db = SQLAlchemy(app)
+app.app_context().push()
+
+
+class Habit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    habit_name = db.Column(db.String(30), unique=True, nullable=False)
+    habit_desc = db.Column(db.String())
+
+    def __repr__(self):
+        return f"Habit('{self.habit_name}', '{self.habit_desc}')"
+
 
 habits = [
     {"name": "Drink Water", "desc": "2l a day"},
