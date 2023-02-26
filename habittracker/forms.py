4fe72d8c31491433ce8d88050acd8de2aa6ac1c3
Oliver Cutting <oliver.cutting@gmail.com>
2023-02-26 13:13:16 +0000

    add registration and login forms

--- habittracker/forms.py
+++ habittracker/forms.py
@@ -1,9 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class HabitInputForm(FlaskForm):
     name = StringField("Habit Name", validators=[DataRequired(), Length(min=2, max=30)])
     desc = StringField("Habit Description")
     submit = SubmitField("Create Habit")
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=2, max=16)]
+    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Sign Up")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Sign Up")
